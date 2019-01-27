9dfc7b7935fd1ecea1cfdcaa8f957ca19c7b5f90
lilla301 <45355384+lilla301@users.noreply.github.com>
2019-01-27 14:07:31 +0100

    Gombok és funkciók az első formon
    Az első formon a gombok eltűnnek, amelyikek még bejelentkezés előtt nem kellene, hogy láthatóak legyenek. Kilépés gomb működik, tooltip működik, ami nem elérhető az nem elérhető

--- MaTaekwonDo/MaTaekwonDo/Index.Designer.cs
+++ MaTaekwonDo/MaTaekwonDo/Index.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MaTaekwonDo
 {
-    partial class Form1
+    partial class Index
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,202 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonWebOldal = new System.Windows.Forms.Button();
+            this.buttonDig = new System.Windows.Forms.Button();
+            this.buttonLicense = new System.Windows.Forms.Button();
+            this.buttonProfil = new System.Windows.Forms.Button();
+            this.toolTipLogin = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonFelhKezelo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(934, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "\"A győzelemért az embernek mindenekelőtt tudnia kell önmagát legyőzni...\"";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(448, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 43);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MaTaekwonDo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(437, 341);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(692, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "\"A harcosnak, ki megacélozza testét, lelke is nemesedik,";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(516, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(752, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "és megvalósítja test, lélek és szellem tökéletes összhangját...\"";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Transparent;
+            this.buttonExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonExit.BackgroundImage")));
+            this.buttonExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Location = new System.Drawing.Point(1228, 9);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(40, 37);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLogin.BackgroundImage")));
+            this.buttonLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Location = new System.Drawing.Point(12, 535);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(191, 173);
+            this.buttonLogin.TabIndex = 9;
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonWebOldal
+            // 
+            this.buttonWebOldal.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonWebOldal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonWebOldal.BackgroundImage")));
+            this.buttonWebOldal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonWebOldal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWebOldal.Location = new System.Drawing.Point(221, 535);
+            this.buttonWebOldal.Name = "buttonWebOldal";
+            this.buttonWebOldal.Size = new System.Drawing.Size(191, 173);
+            this.buttonWebOldal.TabIndex = 10;
+            this.buttonWebOldal.UseVisualStyleBackColor = false;
+            this.buttonWebOldal.Click += new System.EventHandler(this.buttonWebOldal_Click);
+            // 
+            // buttonDig
+            // 
+            this.buttonDig.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonDig.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDig.BackgroundImage")));
+            this.buttonDig.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDig.Location = new System.Drawing.Point(880, 535);
+            this.buttonDig.Name = "buttonDig";
+            this.buttonDig.Size = new System.Drawing.Size(191, 173);
+            this.buttonDig.TabIndex = 11;
+            this.buttonDig.UseVisualStyleBackColor = false;
+            // 
+            // buttonLicense
+            // 
+            this.buttonLicense.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonLicense.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLicense.BackgroundImage")));
+            this.buttonLicense.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLicense.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLicense.Location = new System.Drawing.Point(1077, 535);
+            this.buttonLicense.Name = "buttonLicense";
+            this.buttonLicense.Size = new System.Drawing.Size(191, 173);
+            this.buttonLicense.TabIndex = 12;
+            this.buttonLicense.UseVisualStyleBackColor = false;
+            this.buttonLicense.Click += new System.EventHandler(this.buttonLicense_Click);
+            // 
+            // buttonProfil
+            // 
+            this.buttonProfil.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonProfil.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonProfil.BackgroundImage")));
+            this.buttonProfil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonProfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProfil.Location = new System.Drawing.Point(418, 535);
+            this.buttonProfil.Name = "buttonProfil";
+            this.buttonProfil.Size = new System.Drawing.Size(191, 173);
+            this.buttonProfil.TabIndex = 13;
+            this.buttonProfil.UseVisualStyleBackColor = false;
+            this.buttonProfil.Click += new System.EventHandler(this.buttonProfil_Click);
+            // 
+            // buttonFelhKezelo
+            // 
+            this.buttonFelhKezelo.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.buttonFelhKezelo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonFelhKezelo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFelhKezelo.Location = new System.Drawing.Point(683, 535);
+            this.buttonFelhKezelo.Name = "buttonFelhKezelo";
+            this.buttonFelhKezelo.Size = new System.Drawing.Size(191, 173);
+            this.buttonFelhKezelo.TabIndex = 14;
+            this.buttonFelhKezelo.UseVisualStyleBackColor = false;
+            this.buttonFelhKezelo.Click += new System.EventHandler(this.buttonFelhKezelo_Click);
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.buttonFelhKezelo);
+            this.Controls.Add(this.buttonProfil);
+            this.Controls.Add(this.buttonLicense);
+            this.Controls.Add(this.buttonDig);
+            this.Controls.Add(this.buttonWebOldal);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Index";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MaTaekwonDo";
+            this.Load += new System.EventHandler(this.Index_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonWebOldal;
+        private System.Windows.Forms.Button buttonDig;
+        private System.Windows.Forms.Button buttonLicense;
+        private System.Windows.Forms.Button buttonProfil;
+        private System.Windows.Forms.ToolTip toolTipLogin;
+        private System.Windows.Forms.Button buttonFelhKezelo;
     }
 }
 
--- MaTaekwonDo/MaTaekwonDo/Index.cs
+++ MaTaekwonDo/MaTaekwonDo/Index.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace MaTaekwonDo
 {
-    public partial class Form1 : Form
+    public partial class Index : Form
     {
-        public Form1()
+        public Index()
         {
             InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            //Bejelentkezes b = new Bejelentkezes();
+            //b.Show();
+            this.Hide();
+        }
+
+        private void buttonWebOldal_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jelenleg nem elérhető");
+        }
+
+        private void buttonLicense_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jelenleg nem elérhető");
+        }
+
+        private void buttonProfil_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jelenleg nem elérhető");
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+            toolTipLogin.SetToolTip(buttonDig, "Digitális Anyag");
+            toolTipLogin.SetToolTip(buttonExit, "A program bezárása");
+            toolTipLogin.SetToolTip(buttonLogin, "Bejelentkezés");
+            toolTipLogin.SetToolTip(buttonLicense, "Az alkotóról");
+            toolTipLogin.SetToolTip(buttonWebOldal, "A hivatalos weboldal");
+            toolTipLogin.SetToolTip(buttonProfil, "Felhasználói profil");
+            toolTipLogin.SetToolTip(buttonFelhKezelo, "Felhasználók kezelése");
+            buttonDig.Visible = false;
+            buttonFelhKezelo.Visible = false;
+            buttonLicense.Visible = false;
+            buttonProfil.Visible = false;
+
+        }
+
+        private void buttonFelhKezelo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jelenleg nem elérhető");
         }
     }
 }
